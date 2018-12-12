c0c6964570bd675ba6aeb1ddc22dab14600e8057
Dennis Anderson <askmranderson@gmail.com>
2018-12-12 14:06:29 +0100

    pretty robot

--- app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/robots/MainRobot.kt
+++ app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/robots/MainRobot.kt
@@ -1,26 +1,21 @@
 package com.example.android.testing.espresso.basicSample.robots
 
-import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
 import com.example.android.testing.espresso.basicSample.R
 
+fun main(func: MainRobot.() -> Unit) = MainRobot().apply { func() }
+
 class MainRobot {
 
-    fun enterText(text : String) : MainRobot {
-        Espresso.onView(ViewMatchers.withId(R.id.editTextUserInput)).perform(ViewActions.typeText(text),
-                ViewActions.closeSoftKeyboard())
-        return this
-    }
+    fun enterText(text : String) =
+            onView(ViewMatchers.withId(R.id.editTextUserInput)).perform(ViewActions.typeText(text),
+                    ViewActions.closeSoftKeyboard())
 
-    fun clickTest1() : MainRobot {
-        Espresso.onView(ViewMatchers.withId(R.id.test1)).perform(ViewActions.click())
-        return this
-    }
+    fun clickTest1() = onView(ViewMatchers.withId(R.id.test1)).perform(ViewActions.click())
 
-    fun hasText(text : String) : MainRobot {
-        Espresso.onView(ViewMatchers.withText(text)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        return this
-    }
+    fun hasText(text : String) = onView(ViewMatchers.withText(text)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
 }
